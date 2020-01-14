1dda26c7e259bf2c996ce92bc8cd8b6b17eda195
Eduardo Bouças <mail@eduardoboucas.com>
2020-01-14 14:11:28 +0000

    Add data normalisation and updated setup process

--- index.js
+++ index.js
@@ -1,34 +1,211 @@
 const contentful = require("contentful");
-const package = require("./package.json");
+const contentfulManagement = require("contentful-management");
+const { normalizeEntries } = require("./lib/contentful-util");
+const pkg = require("./package.json");
+
+module.exports.options = {
+  accessToken: {
+    env: "CONTENTFUL_ACCESS_TOKEN",
+    private: true
+  },
+  environment: {},
+  pollingInterval: {
+    default: 5000
+  },
+  projectId: {},
+  watch: {
+    default: false,
+    runtimeParameter: "watch"
+  }
+};
 
 module.exports.bootstrap = async ({
-  getContext,
+  getPluginContext,
   log,
   options,
   refresh,
-  setContext
+  setPluginContext
 }) => {
-  log(`Hello from Contentful!`);
+  const clientManagement = contentfulManagement.createClient({
+    accessToken: options.accessToken
+  });
+  const space = await clientManagement.getSpace(options.spaceId);
+  const { items: apiKeys } = await space.getApiKeys();
+
+  let apiKey = apiKeys.find(({ name }) => name === pkg.name);
+
+  if (!apiKey) {
+    apiKey = await space.createApiKey({
+      name: pkg.name,
+      environments: [
+        {
+          sys: {
+            type: "Link",
+            linkType: "Environment",
+            id: options.environment
+          }
+        }
+      ]
+    });
+  }
+
+  const client = contentful.createClient({
+    accessToken: apiKey.accessToken,
+    space: options.spaceId
+  });
+  const { assets, entries, nextSyncToken } = await client.sync({
+    initial: true,
+    resolveLinks: true
+  });
+
+  setPluginContext({
+    assets,
+    entries,
+    nextSyncToken
+  });
+
+  if (options.watch) {
+    setInterval(async () => {
+      const { assets, entries, nextSyncToken } = getPluginContext();
+      const response = await client.sync({
+        nextSyncToken
+      });
+
+      if (response.nextSyncToken === nextSyncToken) {
+        return;
+      }
+
+      // Handling updated assets.
+      response.assets.forEach(asset => {
+        const index = assets.findIndex(({ sys }) => sys.id === asset.sys.id);
+
+        if (index !== -1) {
+          assets[index] = asset;
+        }
+      });
+
+      // Handling updated entries.
+      response.entries.forEach(entry => {
+        const index = entries.findIndex(({ sys }) => sys.id === entry.sys.id);
+
+        log("Updated entry:");
+        log(entries[index]);
+
+        if (index !== -1) {
+          entries[index] = entry;
+        }
+      });
+
+      setPluginContext({
+        assets,
+        entries,
+        nextSyncToken: response.nextSyncToken
+      });
+
+      refresh();
+    }, options.pollingInterval);
+  }
 };
 
-module.exports.getOptionsFromSetup = ({ accessToken, spaceId }) => {
+module.exports.transform = ({ data, getPluginContext }) => {
+  const { assets = [], entries = [] } = getPluginContext();
+  const normalizedEntries = normalizeEntries(assets, entries);
+
+  return {
+    ...data,
+    objects: data.objects.concat(normalizedEntries)
+  };
+};
+
+module.exports.getOptionsFromSetup = ({
+  accessToken,
+  environment,
+  spaceId
+}) => {
   return {
     accessToken,
+    environment,
     spaceId
   };
 };
 
-module.exports.getSetup = () => {
-  return [
-    {
-      type: "input",
-      name: "spaceId",
-      message: "What is your Contentful space ID?"
-    },
-    {
-      type: "input",
-      name: "accessToken",
-      message: "What is your Contentful access token?"
+module.exports.getSetup = ({ chalk, inquirer, ora }) => {
+  return async () => {
+    const answers = {};
+    const { accessToken } = await inquirer.prompt([
+      {
+        type: "input",
+        name: "accessToken",
+        message:
+          "What is your Contentful Personal Access Token? If you don't have one, see https://www.contentful.com/r/knowledgebase/personal-access-tokens/."
+      }
+    ]);
+
+    answers.accessToken = accessToken;
+
+    const client = contentfulManagement.createClient({
+      accessToken: answers.accessToken
+    });
+    const spacesSpinner = ora("Looking for Contentful spaces").start();
+    const { items: spaces } = await client.getSpaces();
+
+    spacesSpinner.succeed();
+
+    if (spaces.length === 1) {
+      answers.spaceId = spaces[0].sys.id;
+
+      ora(
+        `The only space in the account has been selected: ${chalk.bold(
+          spaces[0].name
+        )}.`
+      ).info();
+    } else {
+      const { spaceId } = await inquirer.prompt([
+        {
+          type: "list",
+          name: "spaceId",
+          message: "Which Contentful space do you want to use?",
+          choices: spaces.map(space => ({
+            name: space.name,
+            value: space.sys.id
+          }))
+        }
+      ]);
+
+      answers.spaceId = spaceId;
     }
-  ];
+
+    const environmentsSpinner = ora("Looking for environments").start();
+    const space = await client.getSpace(answers.spaceId);
+    const { items: environments } = await space.getEnvironments();
+
+    environmentsSpinner.succeed();
+
+    if (environments.length === 1) {
+      answers.environment = environments[0].sys.id;
+
+      ora(
+        `The only environment in the space has been selected: ${chalk.bold(
+          environments[0].name
+        )}.`
+      ).info();
+    } else {
+      const { environment } = await inquirer.prompt([
+        {
+          type: "list",
+          name: "environment",
+          message: "What environment do you want to use?",
+          choices: environments.map(environment => ({
+            name: environment.name,
+            value: environment.sys.id
+          })),
+          default: environments[0].sys.id
+        }
+      ]);
+
+      answers.environment = environment;
+    }
+
+    return answers;
+  };
 };
